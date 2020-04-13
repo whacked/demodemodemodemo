e3d137d43f8bddd02f6c16eb33c50ced09eb10b7
natto <whacked@users.noreply.github.com>
2020-04-12 17:35:48 -0700

    Add disposable jupyter notebook with contrib extensions

---
+++ jupyter-notebook-sandbox-with-extensions/default.nix
@@ -0,0 +1,80 @@
+with import <nixpkgs> {};
+
+let
+in stdenv.mkDerivation rec {
+  name = "jupyter-notebook-sandbox-with-extensions";
+  env = buildEnv {
+    name = name;
+    paths = buildInputs;
+  };
+  buildInputs = [
+    python37Full
+    # python37Packages.jupyter
+  ];
+  shellHook = ''
+    _BASH_SHARED_DIR=$CLOUDSYNC/main/dev/setup/bash
+    . $_BASH_SHARED_DIR/nix_shortcuts.sh
+
+    USERCACHE=''${USERCACHE-/tmp/cache}
+    if [ ! -e $USERCACHE ]; then
+        mkdir -p $USERCACHE
+    fi
+
+    function setup-first-run() {
+        pip install jupyter jupyter_contrib_nbextensions autopep8
+        jupyter contrib nbextension install --user
+        ENABLE_BUNDLED_EXTENSIONS=(
+            code_prettify/autopep8
+            codefolding/main
+            collapsible_headings/main
+            contrib_nbextensions_help_item/main
+            datestamper/main
+            execute_time/ExecuteTime
+            freeze/main
+            scratchpad/main
+            toc2/main
+            toggle_all_line_numbers/main
+            varInspector/main
+        );
+        for ext in ''${ENABLE_BUNDLED_EXTENSIONS[@]}; do
+            jupyter nbextension enable $ext
+        done
+    }
+
+    function run-unprotected-server() {
+        _token=''${1-asdf}
+        jupyter notebook --no-browser --ip=0.0.0.0 --NotebookApp.token=$_token
+    }
+
+    unset PYTHONPATH
+    # FIX for ImportError: libstdc++.so.6: cannot open shared object file: No such file or directory
+    # when starting jupyter from nix-shell
+    if [ -e /usr/lib/x86_64-linux-gnu ]; then
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu
+    fi
+
+    export VIRTUAL_ENV=''${VIRTUAL_ENV-$USERCACHE/$name-venv}
+
+    if [ "x$JUPYTER_CONFIG_DIR" == "x" ]; then
+        ALTHOME=$USERCACHE/$name-home
+        mkdir -p $ALTHOME
+        export JUPYTER_CONFIG_DIR=$ALTHOME/jupyter
+        export IPYTHONDIR=''${IPYTHONDIR-$ALTHOME/ipython}
+        echo "setting jupyter working directory to $JUPYTER_CONFIG_DIR"
+        echo "setting python working directory to $IPYTHONDIR"
+        
+        JUPYTER_WORK_DIR=$USERCACHE/$name-jupyter-work
+        export JUPYTER_DATA_DIR=$JUPYTER_WORK_DIR/data
+        export JUPYTER_RUNTIME_DIR=$JUPYTER_WORK_DIR/runtime
+    fi
+
+    if [ ! -e $VIRTUAL_ENV ]; then
+        python -m venv $VIRTUAL_ENV
+        source $VIRTUAL_ENV/bin/activate
+        setup-first-run
+    else
+        source $VIRTUAL_ENV/bin/activate
+    fi
+  '';
+}
+
