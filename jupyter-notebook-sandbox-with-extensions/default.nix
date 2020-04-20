2affdaeaf68a4bff0d3bc6d27bb8530b29c1461a
natto <whacked@users.noreply.github.com>
2020-04-19 19:24:40 -0700

    add `run` handy alias

--- jupyter-notebook-sandbox-with-extensions/default.nix
+++ jupyter-notebook-sandbox-with-extensions/default.nix
@@ -106,6 +106,7 @@ in stdenv.mkDerivation rec {
     
     alias build-docker-container='sudo $(which docker) build . -t emacs-with-nix'
     alias run-nix-docker-container='sudo $(which docker) run -p 8888:8888 -v $PWD:/opt/demo -w /opt/demo --rm -it emacs-with-nix /bin/bash'
+    alias run='jupyter notebook --no-browser'
     cat default.nix | grep '\(alias\|function\) .*'
   '';
 }
