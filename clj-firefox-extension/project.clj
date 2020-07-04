d0a228368fd0db64bd69623920eec69e92f81379
whacked <whacked@users.noreply.github.com>
2020-07-03 19:11:57 -0700

    add luminus template generated base project.clj
    command:
    lein new luminus clj-firefox-extension +sqlite +shadow-cljs +reagent

---
+++ clj-firefox-extension/project.clj
@@ -0,0 +1,114 @@
+(defproject clj-firefox-extension "0.1.0-SNAPSHOT"
+
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[ch.qos.logback/logback-classic "1.2.3"]
+                 [cheshire "5.10.0"]
+                 [cljs-ajax "0.8.0"]
+                 [clojure.java-time "0.3.2"]
+                 [com.cognitect/transit-clj "1.0.324"]
+                 [com.fasterxml.jackson.core/jackson-core "2.11.1"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.11.1"]
+                 [com.google.javascript/closure-compiler-unshaded "v20200504" :scope "provided"]
+                 [conman "0.9.0"]
+                 [cprop "0.1.17"]
+                 [expound "0.8.5"]
+                 [funcool/struct "1.4.0"]
+                 [luminus-migrations "0.6.7"]
+                 [luminus-transit "0.1.2"]
+                 [luminus-undertow "0.1.5"]
+                 [luminus/ring-ttl-session "0.3.3"]
+                 [markdown-clj "1.10.4"]
+                 [metosin/jsonista "0.2.6"]
+                 [metosin/muuntaja "0.6.7"]
+                 [metosin/reitit "0.5.2"]
+                 [metosin/ring-http-response "0.9.1"]
+                 [mount "0.1.16"]
+                 [nrepl "0.7.0"]
+                 [org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.773" :scope "provided"]
+                 [org.clojure/core.async "1.2.603"]
+                 [org.clojure/google-closure-library "0.0-20191016-6ae1f72f" :scope "provided"]
+                 [org.clojure/google-closure-library-third-party "0.0-20191016-6ae1f72f" :scope "provided"]
+                 [org.clojure/tools.cli "1.0.194"]
+                 [org.clojure/tools.logging "1.1.0"]
+                 [org.webjars.npm/bulma "0.8.2"]
+                 [org.webjars.npm/material-icons "0.3.1"]
+                 [org.webjars/webjars-locator "0.40"]
+                 [org.webjars/webjars-locator-jboss-vfs "0.1.0"]
+                 [org.xerial/sqlite-jdbc "3.32.3"]
+                 [reagent "0.10.0"]
+                 [ring-webjars "0.2.0"]
+                 [ring/ring-core "1.8.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [selmer "1.12.27"]
+                 [thheller/shadow-cljs "2.10.13" :scope "provided"]]
+
+  :min-lein-version "2.0.0"
+  
+  :source-paths ["src/clj" "src/cljs" "src/cljc"]
+  :test-paths ["test/clj"]
+  :resource-paths ["resources" "target/cljsbuild"]
+  :target-path "target/%s/"
+  :main ^:skip-aot clj-firefox-extension.core
+
+  :plugins [[lein-shadow "0.2.0"]] 
+  :clean-targets ^{:protect false}
+  [:target-path "target/cljsbuild"]
+  :shadow-cljs
+  {:nrepl {:port 7002}
+   :builds
+   {:app
+    {:target :browser
+     :output-dir "target/cljsbuild/public/js"
+     :asset-path "/js"
+     :modules {:app {:entries [clj-firefox-extension.app]}}
+     :devtools {:watch-dir "resources/public"}}
+    :test
+    {:target :node-test
+     :output-to "target/test/test.js"
+     :autorun true}}}
+  
+  :npm-deps [[shadow-cljs "2.10.13"]
+             [create-react-class "15.6.3"]
+             [react "16.8.6"]
+             [react-dom "16.8.6"]]
+  :npm-dev-deps [[xmlhttprequest "1.8.0"]]
+
+  :profiles
+  {:uberjar {:omit-source true
+             :prep-tasks ["compile" ["shadow" "release" "app"]]
+             
+             :aot :all
+             :uberjar-name "clj-firefox-extension.jar"
+             :source-paths ["env/prod/clj"  "env/prod/cljs" ]
+             :resource-paths ["env/prod/resources"]}
+
+   :dev           [:project/dev :profiles/dev]
+   :test          [:project/dev :project/test :profiles/test]
+
+   :project/dev  {:jvm-opts ["-Dconf=dev-config.edn" ]
+                  :dependencies [[binaryage/devtools "1.0.2"]
+                                 [cider/piggieback "0.5.0"]
+                                 [pjstadig/humane-test-output "0.10.0"]
+                                 [prone "2020-01-17"]
+                                 [ring/ring-devel "1.8.1"]
+                                 [ring/ring-mock "0.4.0"]]
+                  :plugins      [[com.jakemccrary/lein-test-refresh "0.24.1"]
+                                 [jonase/eastwood "0.3.5"]] 
+                  
+                  
+                  :source-paths ["env/dev/clj"  "env/dev/cljs" "test/cljs" ]
+                  :resource-paths ["env/dev/resources"]
+                  :repl-options {:init-ns user
+                                 :timeout 120000}
+                  :injections [(require 'pjstadig.humane-test-output)
+                               (pjstadig.humane-test-output/activate!)]}
+   :project/test {:jvm-opts ["-Dconf=test-config.edn" ]
+                  :resource-paths ["env/test/resources"] 
+                  
+                  
+                  }
+   :profiles/dev {}
+   :profiles/test {}})
