04734a8fd7d0a19336afe514796478fcc4da6d27
whacked <whacked@users.noreply.github.com>
2020-09-06 10:28:32 -0700

    fix imports in electron version

--- electron-golden-layout-xtermjs/src/app/renderer.cljs
+++ electron-golden-layout-xtermjs/src/app/renderer.cljs
@@ -2,6 +2,7 @@
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [reagent.core :as r]
             [wksymclj.ui.golden-layout :as wk-gl]
+            [app.xterm :as axt]
             ["xterm" :as xterm]
             ["xterm-addon-fit" :as xterm-addon-fit]
             ["xterm-addon-web-links" :as xterm-addon-web-links]
@@ -16,11 +17,6 @@
 (def $config (as-> "resources/config.edn" $
                (.readFileSync fs $ "utf-8")
                (cljs.reader/read-string $)))
-
-(def $process-out-channel (chan))
-
-(defn nl2cr [s]
-  (clojure.string/replace s "\n" "\r\n"))
 
 (defn exec-process! [cmd & {:keys [stdout stderr exit]}]
   (let [proc (spawn "bash" (clj->js ["-c" cmd]))
@@ -39,8 +35,8 @@
   (let [t0 (js/Date.)
         push-to-term! (fn [data]
                         (go
-                          (>! $process-out-channel
-                              (-> data str nl2cr))))]
+                          (>! axt/$process-out-channel
+                              (-> data str axt/nl2cr))))]
     (exec-process!
       cmd
       :stdout push-to-term!
@@ -58,116 +54,72 @@
                         "\n")
                    (push-to-term!))))))
 
-(defn setup-terminal! [container]
-  (gdom/removeChildren container)
-  (let [term (new (aget xterm "Terminal"))
-        fit-addon (new (aget xterm-addon-fit "FitAddon"))
-        web-links-addon (new (aget xterm-addon-web-links "WebLinksAddon"))
-
-        input-buffer (atom nil)
-        ps1 (str "Hello from " (->> "xterm.js"
-                                 (.red chalk)
-                                 (.bold chalk)
-                                 (.italic chalk)) " $ ")
-        exec! (fn [cmd]
-                (let [t0 (js/Date.)
-                      write-to-term! (fn [data] (.write term (nl2cr (str data))))]
-                  (exec-process!
-                    cmd
-                    :stdout write-to-term!
-                    :stderr write-to-term!
-                    :exit
-                    (fn [code]
-                      (.write term
-                              (str "complete: " cmd "\r\n"
-                                   (/ (- (js/Date.) t0) 1000.0) "s\r\n"))
-                      (.write term ps1)))))
-        ]
-    (doto term
-      (.setOption "disableStdin" true)
-      (.loadAddon fit-addon)
-      (.loadAddon web-links-addon)
-      (.open container)
-      (.write ps1)
-      (.onKey
-        (fn [ev]
-          (try
-            (let [key-code (aget ev "domEvent" "which")
-                  key-char (aget ev "key")]
-              (if (= key-code 13)
-                (let [cur-command (some-> @input-buffer
-                                          (clojure.string/trim)
-                                          (or ""))]
-                  (reset! input-buffer "")
-                  (if (= cur-command "clear")
-                    (do
-                      (js/console.clear)
-                      (.write term "\u001Bc")  ;; clears out dangling "clear" text in the terminal
-                      (.clear term)
-                      (.write term ps1))
-                    (do
-                      (exec! cur-command)
-                      (.writeln term "\n"))))
-                (do
-                  (.write term key-char)
-                  (swap! input-buffer str key-char))
-                ))
-            (catch js/Object error
-              (js/console.warn "onKey error:")
-              (js/console.warn error)
-              (js/console.info ev))))))
-    (.fit fit-addon)
-    (go-loop
-      []
-      (when-let [data (<! $process-out-channel)]
-        (.write term (nl2cr (str data)))
-        (recur)))))
-
 (defn initialize-panels! []
   (let [state (r/atom {:acontent "apple"
                        :bcontent "banana"})]
 
     (r/render
-      [(fn []
-         [:h1 (get-in @state [:acontent])] )]
-      (gdom/getElement "panel-A"))
+     [(fn []
+        [:h1 (get-in @state [:acontent])] )]
+     (gdom/getElement "panel-A"))
     (r/render
-      [(fn []
-         [:h1 (get-in @state [:bcontent])] )]
-      (gdom/getElement "panel-B"))
+     [(fn []
+        [:h1 (get-in @state [:bcontent])] )]
+     (gdom/getElement "panel-B"))
     (r/render
-      [(fn []
-         [:div
-          [:input
-           {:value (get-in @state [:acontent])
-            :on-change (fn [evt]
-                         (swap! state assoc :acontent (aget evt "target" "value")))}]
-          [:input
-           {:value (get-in @state [:bcontent])
-            :on-change (fn [evt]
-                         :value (get-in @state [:bcontent])
-                         (swap! state assoc :bcontent (aget evt "target" "value")))}]]
-         )]
-      (gdom/getElement "panel-D")))
-  (setup-terminal! (gdom/getElement "panel-C"))
+     [(fn []
+        [:div
+         [:input
+          {:value (get-in @state [:acontent])
+           :on-change (fn [evt]
+                        (swap! state assoc :acontent (aget evt "target" "value")))}]
+         [:input
+          {:value (get-in @state [:bcontent])
+           :on-change (fn [evt]
+                        :value (get-in @state [:bcontent])
+                        (swap! state assoc :bcontent (aget evt "target" "value")))}]]
+        )]
+     (gdom/getElement "panel-D")))
+  (axt/setup-terminal!
+   (gdom/getElement "panel-C")
+   (fn [term cmd]
+     (let [write-to-term!
+           (fn [data]
+             (.write term (axt/nl2cr (str data))))
+           t0 (js/Date.)]
+       (exec-process!
+        cmd
+        :stdout write-to-term!
+        :stderr write-to-term!
+        :exit
+        (fn [code]
+          (write-to-term!
+           (str "complete: " cmd "\r\n"
+                (/ (- (js/Date.) t0) 1000.0) "s\r\n"))
+          (write-to-term!
+           (str 
+            (->> "[term-prefix]"
+                 (.red chalk)
+                 (.bold chalk)
+                 (.italic chalk)) " $ ")))))))
 
   (let [state (r/atom {:input nil})]
     (r/render
-      [(fn []
-         [:div
-          {:style {:width "100%"
-                   :padding "0.5em"}}
-          [:input
-           {:style {:width "100%"}
-            :placeholder "type command and hit enter"
-            :value (get-in @state [:input] "")
-            :on-change (fn [ev]
-                         (swap! state assoc-in [:input] (aget ev "target" "value")))
-            :on-key-down (fn [ev]
+     [(fn []
+        [:div
+         {:style {:width "100%"
+                  :padding "0.5em"}}
+         [:input
+          {:style {:width "100%"}
+           :placeholder "type command and hit enter"
+           :value (get-in @state [:input] "")
+           :on-change (fn [ev]
+                        (swap! state assoc-in [:input] (aget ev "target" "value")))
+           :on-key-down (fn [ev]
                           (when (= (aget ev "keyCode") 13)
                             (execute-process-capture-stdout! (get-in @state [:input]))
                             (swap! state assoc-in [:input] "")))}]])]
-      (gdom/getElement "panel-F"))))
+     (gdom/getElement "panel-F"))))
 
 (defn start []
   (js/console.log "renderer - start")
